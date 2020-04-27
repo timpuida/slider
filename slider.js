f178b3444da8e03a62a996eefde67ac53e71c1b5
timpuyda <timpuyda@gmail.com>
2020-04-27 16:00:57 +0300

    commit

---
+++ slider.js
@@ -0,0 +1,37 @@
+﻿new Vue({
+  el:'#app',
+  data: function(){
+    return{
+      currentSlide:0,
+      slides:[
+            {img: '1.jpg',
+              id:0
+            },
+            {
+                img: '2.jpg',
+                id:1
+            },
+            {
+                img: '3.jpg',
+                id: 2
+            }
+          ],
+    }
+  },
+  methods: {
+    nextSlide(){
+      if (this.currentSlide < this.slides.length-1) {
+        this.currentSlide++
+      }else {
+         this.currentSlide = 0
+      }
+    },
+    prevSlide(){
+      if (this.currentSlide > 0) {
+        this.currentSlide--
+      }else {
+        this.currentSlide = this.slides.length - 1
+      }
+    }
+  },
+})
